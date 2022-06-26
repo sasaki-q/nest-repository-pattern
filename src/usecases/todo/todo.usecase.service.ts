9b37a24d7eb25e180d1e6ff2820778ed34785c94
sasaki-q <teppei.sasaki@relic.co.jp>
2022-06-26 14:09:54 +0900

    Add todo repository

--- src/usecases/todo/todo.usecase.service.ts
+++ src/usecases/todo/todo.usecase.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
+import { Todo } from "domains/todo";
+import { MyRepository } from "repositories/repository";
 
 @Injectable()
-export class TodoUsecaseService {}
+export class TodoUsecaseService {
+    constructor (
+        @Inject(MyRepository<Todo>)
+        private readonly repo: MyRepository<Todo>
+    ){}
+}
